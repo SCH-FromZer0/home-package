f34964204e3350e6d9a9eedf2cdd4d73d23a33b3
jeongjy0317 <jeongjy0317@gmail.com>
2024-06-14 22:22:30 +0900

    fix: fixed some issues when check installation

--- setup.sh
+++ setup.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
-if ! command -v docker >/dev/null 2>&1; then
-    echo "[Check Fail] Docker Not Installed"
-    exit 1
+COLOR_RED="\033[1;31m"
+COLOR_GREEN="\033[1;32m"
+COLOR_YELLOW="\033[1;33m"
+COLOR_END="\033[0m"
+
+if (( $EUID != 0 )); then
+  echo -e "$COLOR_RED [ FATAL ]$COLOR_END This script must be run as root"
+  exit 1
 fi
 
-if ! command -v docker-compose >/dev/null 2>&1; then
-    echo "[Check Fail] Docker Compose Not Installed"
+echo -e "$COLOR_YELLOW [ CHECK ]$COLOR_END Checking Docker..."
+if ! /bin/bash ./docker_installer.sh; then
+  if ! command -v docker >/dev/null 2>&1; then
+    echo -e "$COLOR_RED [ ERROR ]$COLOR_END Error while configuring docker install source"
     exit 1
+  fi
 fi
 
 services=("client" "server")
 
 for dir in "${services[@]}"; do
-    echo "[Setup] Processing $dir directory..."
+    echo -e "$COLOR_YELLOW [ SETUP ]$COLOR_END Processing $dir directory..."
 
     # Change to the directory
-    if ! cd ./$dir; then
-        echo "[Setup Failed] Failed to change directory to $dir"
+    if ! cd "./$dir"; then
+        echo -e "$COLOR_RED [ FATAL ]$COLOR_END Failed to change directory to $dir"
         exit 1
     fi
 
     # Run docker-compose up
-    if ! docker-compose up -d; then
-        echo "[Setup Failed] Failed to start Docker Compose in $dir"
+    if ! docker compose up -d; then
+        echo -e "$COLOR_RED [ FATAL ]$COLOR_END Failed to start Docker Compose for $dir"
         exit 1
     fi
 
     # Print success message
-    echo "[Setup Done] $dir started successfully"
+    echo -e "$COLOR_GREEN [ LGTM ]$COLOR_END $dir started successfully"
 
     # Go back to the original directory
     cd ../;
--- start.sh
+++ start.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+COLOR_RED="\033[1;31m"
+COLOR_GREEN="\033[1;32m"
+COLOR_YELLOW="\033[1;33m"
+COLOR_END="\033[0m"
+
 if (( $EUID != 0 )); then
-  echo "[Privilege Error] This script must be run as root"
+  echo -e "$COLOR_RED [ FATAL ]$COLOR_END This script must be run as root"
   exit 1
+fi
+
+echo -e "$COLOR_YELLOW [ CHECK ]$COLOR_END Checking Docker..."
+if ! /bin/bash ./docker_installer.sh; then
+  if ! command -v docker >/dev/null 2>&1; then
+    echo -e "$COLOR_RED [ ERROR ]$COLOR_END Error while configuring docker install source"
+    exit 1
+  fi
 fi
 
 /bin/bash ./update.sh
