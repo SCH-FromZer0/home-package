fa98bf34a518b25d86857a7618f17759c96796c5
jeongjy0317 <jeongjy0317@gmail.com>
2024-06-14 23:30:46 +0900

    mod: updated server

--- update.sh
+++ update.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+COLOR_RED="\033[1;31m"
+COLOR_GREEN="\033[1;32m"
+COLOR_YELLOW="\033[1;33m"
+COLOR_END="\033[0m"
+
 if ! command -v git >/dev/null 2>&1; then
-    echo "[Check Fail] Git Not Installed"
+    echo -e "$COLOR_RED [ ERROR ]$COLOR_END Git Not Installed"
     exit 1
 fi
 
 services=("client" "server")
 for dir in "${services[@]}"; do
-  echo "[Updater] Processing $dir ..."
+  echo -e "$COLOR_YELLOW [ UPDATER ]$COLOR_END Processing $dir ..."
   if ! git submodule update --remote "$dir"; then
-    echo "[Update Failed] Failed to update $dir"
+    echo -e "$COLOR_RED [ ERROR ]$COLOR_END Failed to update $dir"
     exit 1;
   fi
 
   # Print success message
-  echo "[Updated] $dir updated successfully"
+  echo -e "$COLOR_GREEN [ LGTM ]$COLOR_END $dir updated successfully"
 done
